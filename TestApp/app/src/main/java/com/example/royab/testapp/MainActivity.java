299691430d5ed359fc4ec440ce4faabf2ea3a2d2
Abhra Kanti Roy <royabhrakanti@gmail.com>
2016-09-25 14:21:43 +0530

    added Addition

--- TestApp/app/src/main/java/com/example/royab/testapp/MainActivity.java
+++ TestApp/app/src/main/java/com/example/royab/testapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.royab.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText firstNo = (EditText) findViewById(R.id.firstNo);
+        final EditText secondNo = (EditText) findViewById(R.id.secondNo);
+        final TextView ansBox = (TextView) findViewById(R.id.ansBox);
+
+        Button signButton = (Button) findViewById(R.id.addButton);
+
+        signButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                double ans = 0.0;
+                ans = Double.valueOf(firstNo.getText().toString()) + Double.valueOf(secondNo.getText().toString());
+
+                ansBox.setText(String.valueOf(ans));
+
+            }
+        });
     }
 }
